81f578c5acb523b0ab302091f2279c06cd3e2480
Muriel Gonzalez <murielgonzalez@gmail.com>
2020-11-15 12:22:56 -0600

    Combining multiple converters into a custom factory

---
+++ AsteroidRadarApp/app/src/main/java/com/gonzoapps/asteroidradar/network/Annotations.kt
@@ -0,0 +1,13 @@
+package com.gonzoapps.asteroidradar.network
+
+@Target(AnnotationTarget.FUNCTION,
+        AnnotationTarget.PROPERTY_GETTER,
+        AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class Json
+
+@Target(AnnotationTarget.FUNCTION,
+        AnnotationTarget.PROPERTY_GETTER,
+        AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class Scalar
--- AsteroidRadarApp/app/src/main/java/com/gonzoapps/asteroidradar/network/Service.kt
+++ AsteroidRadarApp/app/src/main/java/com/gonzoapps/asteroidradar/network/Service.kt
@@ -5,15 +5,19 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.lang.reflect.Type
 
 interface NasaApiService {
+    @Scalar
     @GET("neo/rest/v1/feed")
     suspend fun getNEoWsListAsync(
         @Query("start_date")
@@ -24,6 +28,7 @@ interface NasaApiService {
         key: String,
     ) : Response<String>
 
+    @Json
     @GET("planetary/apod")
     suspend fun getPictureOfTheDayAsync(
         @Query("date")
@@ -53,9 +58,23 @@ object NasaApi {
 
     val retrofitService:NasaApiService by lazy {
         retrofit
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addConverterFactory(HandleScalarAndJsonConverterFactory.create())
             .build().create(NasaApiService::class.java)
     }
 }
 
+class HandleScalarAndJsonConverterFactory : Converter.Factory() {
+    override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, *>? {
+        annotations.forEach { annotation ->
+            return when (annotation) {
+                is Scalar -> ScalarsConverterFactory.create().responseBodyConverter(type, annotations, retrofit)
+                is Json -> MoshiConverterFactory.create(moshi).responseBodyConverter(type, annotations, retrofit)
+                else -> null
+            }
+        }
+        return null
+    }
+    companion object {
+        fun create()  = HandleScalarAndJsonConverterFactory()
+    }
+}
